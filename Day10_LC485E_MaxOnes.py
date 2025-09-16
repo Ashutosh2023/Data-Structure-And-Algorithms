a25dbe82d9cf910e687d225c93bc630eedba5572
“Ashutosh <singhashutoshkumar2023@gmail.com>
2025-09-16 23:53:30 +0530

    Day 10 Array

--- Day10_LC485E_MaxOnes.py
+++ Day10_LC485E_MaxOnes.py
@@ -1,4 +1,12 @@
-def MaxOnes():
+def findMaxConsecutiveOnes(arr):
+    max_count = 0
+    count = 0
+    for num in arr:
+        if num == 1:
+            count += 1
+            max_count = max(max_count, count)
+        else:
+            count = 0
+    print(max_count) 
 
-
-print(MaxOnes())
+findMaxConsecutiveOnes([1,0,1,1,1,1,1,0,1])
