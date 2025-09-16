ae4f2026f060b0fbefced5f085454682c6209af2
“Ashutosh <singhashutoshkumar2023@gmail.com>
2025-09-17 01:38:07 +0530

    Day 10 code solutions

--- Day10_LC268E_Missing_Number.py
+++ Day10_LC268E_Missing_Number.py
@@ -1,4 +1,7 @@
-def MissingNumbers():
+def MissingNumbers(nums: list[int]):
+    sumVal = sum(nums)
+    n = len(nums)
+    mathVal = (n*(n+1))//2
+    return mathVal - sumVal
 
-
-print(MissingNumbers())
+print(MissingNumbers([9,6,4,2,3,5,7,0,1]))
--- Day10_LC283E_MoveZeros.py
+++ Day10_LC283E_MoveZeros.py
@@ -1,4 +1,27 @@
-def MoveZeros():
+def MoveZeros(arr: list[int]):
+    i = 0
+    j = 0
+    while j<len(arr):
+        if arr[j] != 0:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+        if arr[i] != 0:
+            i+=1
+            j+=1
+            continue
+        elif arr[i] == 0:
+            j+=1
+        
+    print(arr)
 
+MoveZeros([0,1,0,3,12])
 
-print(MoveZeros())
+def moveZeroesOpt(arr: list[int]) -> None:
+    insert_pos = 0
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr[i], arr[insert_pos] = arr[insert_pos], arr[i]
+            insert_pos += 1
+    print(arr)
+
+moveZeroesOpt([0,1,0,3,12])
