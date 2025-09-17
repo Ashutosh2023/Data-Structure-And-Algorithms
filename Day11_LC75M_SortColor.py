304a99fe7a5eaee040d1ad8a5dcfa2f5d8b747f3
“Ashutosh <singhashutoshkumar2023@gmail.com>
2025-09-18 00:55:57 +0530

    day 11 dutch nationl flag algo

--- Day11_LC75M_SortColor.py
+++ Day11_LC75M_SortColor.py
@@ -1,19 +1,18 @@
-class Solution:
-    def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+def sortColors(arr: list[int]) -> None:
         i = 0
-        j = len(nums)-1
         k = 0
-        while i<j:
-            if arr[i] == 0:
-                k +=1
-                break
-            if arr[j] == 2:
+        j = len(arr)-1
+        while k<=j:
+            if arr[k] == 0:
+                arr[i], arr[k] = arr[k], arr[i]
+                i += 1
+                k += 1
+            elif arr[k] == 1:
+                k += 1
+            else:
+                arr[k], arr[j] = arr[j], arr[k]
                 j -= 1
-                break
-            if arr[k] = 1
 
-
-            
+arr = [0,1,0,2,1,0,2]
+sortColors(arr)
+print(arr)
