8072b4171e6251b70721fa6da7d44c1f57183b63
“Ashutosh <singhashutoshkumar2023@gmail.com>
2025-09-19 23:22:16 +0530

    Day 13 array questions

---
+++ Day13_LC121E_BuySellStock.py
@@ -0,0 +1,7 @@
+#Best time to buy and sell stock:
+def buySellStock(prices: list[int]) -> int:
+
+
+
+prices = [7,1,5,3,6,4]
+print(buySellStock(prices))
---
+++ Day13_LC560M_SubarraySum_K.py
@@ -0,0 +1,7 @@
+def subarraySumEqualsK(nums: list[int], k: int) -> int:
+
+
+
+nums = [1,1,1]
+k = 2
+print(subarraySumEqualsK(nums, k))
