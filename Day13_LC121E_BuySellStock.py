dd07615431ccb4a20f97915159e48cb2e67cc12a
“Ashutosh <singhashutoshkumar2023@gmail.com>
2025-09-20 22:51:15 +0530

    Day 13 buy sell stock

--- Day13_LC121E_BuySellStock.py
+++ Day13_LC121E_BuySellStock.py
@@ -1,7 +1,30 @@
 #Best time to buy and sell stock:
 def buySellStock(prices: list[int]) -> int:
+    n = len(prices)
+    maxDiff = 0
+    for i in range(n):
+        for j in range(i, n):
+            diff = prices[j]-prices[i]
+            maxDiff = max(diff, maxDiff)
+    return maxDiff
 
 
+prices = [7,6,4,3,1]
+# print(buySellStock(prices))
 
-prices = [7,1,5,3,6,4]
-print(buySellStock(prices))
+
+def buySellStock2(prices: list[int]) -> int:
+    minPrice = 2**31 -1
+    maxProfit = 0
+
+    for price in prices:
+        if price < minPrice:
+            minPrice = price
+        profit = price - minPrice
+        if profit > maxProfit:
+            maxProfit = profit
+    return maxProfit
+
+
+prices = [7,6,4,3,1]
+print(buySellStock2(prices))
