15f2a3377d73d4040272333f6e2bf06f6849c2a2
“Ashutosh <singhashutoshkumar2023@gmail.com>
2025-09-22 23:17:10 +0530

    Day 16 Rotate Array

---
+++ Day16_LC48M_RotateImage.py
@@ -0,0 +1,11 @@
+def rotateMatrix(matrix: list[list[int]]) -> None:
+    matrix.reverse()
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i,n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+# matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+rotateMatrix(matrix)
+print(matrix)
