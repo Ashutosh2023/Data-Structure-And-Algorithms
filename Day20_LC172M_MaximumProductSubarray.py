e3722099282136a9f6dadeb8d7795d389d8241a2
“Ashutosh <singhashutoshkumar2023@gmail.com>
2025-09-26 23:59:24 +0530

    Day 20 maxproduct subarray

---
+++ Day20_LC172M_MaximumProductSubarray.py
@@ -0,0 +1,37 @@
+#My code::Wrong fails for -ve values..
+def maxProductSubarray(nums: list[int]) -> int:
+    maxProduct = 0
+    currProduct = 1
+    for i in range(len(nums)):
+        if nums[i] == 0:
+            currProduct = 1
+            continue
+        currProduct = currProduct * nums[i]
+        maxProduct = max(maxProduct, currProduct)
+    return maxProduct
+
+# # nums = [-2,0,-1]
+# nums = [2,3,4,0,6,6]
+# # nums = [-2,3,-4]
+# print(maxProductSubarray(nums))
+
+
+def maxProductSubarray(nums: list[int]) -> int:
+    prefix = suffix = 1
+    maxProd = 0
+    n = len(nums)
+    for i in range(n):
+        if(prefix == 0):
+            prefix = 1
+        if(suffix == 0):
+            suffix = 1
+        prefix = prefix * nums[i]
+        suffix = suffix * nums[n - i -1]
+        maxProd = max(maxProd, max(prefix, suffix))
+        print(prefix,suffix,maxProd)
+    return maxProd
+
+nums = [-2,0,-1]
+nums = [2,3,-2,4]
+nums = [-2,-3,-4]
+print(maxProductSubarray(nums))
