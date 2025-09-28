ccaf3338fcbc7fbd24fdf0d4bd398e92be0b0604
“Ashutosh <singhashutoshkumar2023@gmail.com>
2025-09-29 00:27:02 +0530

    Day 17 3 sum brute and optimized

--- Day17_LC15M_3Sum.py
+++ Day17_LC15M_3Sum.py
@@ -21,25 +21,25 @@ def threeSumOpt(nums: list[int]) -> list[list[int]]:
     nums.sort()
     n = len(nums)
     result = []
-    for i in range(n-2):
-        if i>0 and nums[i] == nums[i-1]:
+    for i in range(n-2):                   ## fix the i change j and k
+        if i>0 and nums[i] == nums[i-1]:   ## if two index are equivalent then continue and if not the first index(else will cause index error)
             continue
         j = i+1
         k = n-1
-        while j<k:
+        while j<k:                         ## change j and k value to get 0
             sum = nums[i] + nums[j] + nums[k]
-            if sum < 0:
-                j += 1
-            elif sum > 0:
-                k -= 1
+            if sum < 0:                    ## if sum<0 then increase the sum, how increase j as array is sorted in ascnding order
+                j += 1                     ## so the intention is to get j+1 th value to get the bigger value since sum<0
+            elif sum > 0:                  ## if sum>0 then decrease sum to get 0
+                k -= 1                     ## do that by decreasing k to get a smaller value
             else:
                 temp = [nums[i], nums[j], nums[k]]
                 result.append(temp)
                 j += 1
                 k -= 1
-                while j < k and nums[j] == nums[j-1]:
-                    j += 1
-                while k > j and nums[k] == nums[k+1]:
+                while j < k and nums[j] == nums[j-1]: ## since array can have duplicate values, nums[i] = -1, j = 0, k = 1
+                    j += 1                            ## sum is 0 but after i+1, if nums[i+1] = -1 same as nums[i] then will again get the same set
+                while k > j and nums[k] == nums[k+1]: ## but we want the distinct triplets
                     k -= 1
     return result
 
