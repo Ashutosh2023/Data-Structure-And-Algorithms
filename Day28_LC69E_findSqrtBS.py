239a3115555c79eac2c89d1cbf539370d969aeee
“Ashutosh <singhashutoshkumar2023@gmail.com>
2025-10-04 23:51:16 +0530

    Find Sqrt using Binary Search

---
+++ Day28_LC69E_findSqrtBS.py
@@ -0,0 +1,39 @@
+def SearchSqrt(x):
+    if x < 2:
+        return x
+    left, right = 2, x // 2
+    while left <= right:
+        mid = left + (right - left) // 2
+        num = mid * mid
+        if num == x:
+            return mid
+        elif num < x:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return right
+
+# print(SearchSqrt(8))
+
+# Time Complexity: O(log n) : recursion
+def SqrtBinarySearch(x: int, low: int, high: int) -> int:
+    if low == high:
+        return low
+    mid = low + (high - low) // 2
+    num = mid ** 2
+    if num == x:
+        return mid
+    if num > x:
+        high = mid-1
+    else:
+        low = mid+1
+    return SqrtBinarySearch(x, low, high)
+    
+    
+def FindSqrt(x: int) -> int:
+    if x < 2:
+        return x
+    else:
+        return SqrtBinarySearch(x, 2, x // 2)
+
+print(FindSqrt(8))
