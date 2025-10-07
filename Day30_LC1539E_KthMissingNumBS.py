0c4a5702b9cb87e2ac71a846351d9bea1f4cdaed
“Ashutosh <singhashutoshkumar2023@gmail.com>
2025-10-07 23:58:16 +0530

    Day 30

---
+++ Day30_LC1539E_KthMissingNumBS.py
@@ -0,0 +1,33 @@
+# 0ms Beats 100%  Time: O(n)
+def missingNum(nums: list[int], k: int) -> int:
+    n = len(nums)
+    count = 0
+    counter = 1
+    i = 0
+    while i<n and count<k:
+        if nums[i] > counter:
+            count += 1
+        elif nums[i] == counter:
+            i += 1
+        counter += 1
+    return counter+k-count-1
+
+nums = [5,7,10,12]
+k = 5
+# print(missingNum(nums, k))
+
+def missingNum2(nums: list[int], k: int, low: int, high: int) -> int:
+    while low<=high:
+        mid = low + (high - low)//2
+        missingNums = nums[mid]-(mid+1)
+        # print(low, high, mid, "::",nums[mid],"-", mid+1, "=", nums[mid]-(mid+1))
+        if missingNums < k:
+            low = mid + 1
+        else:
+            high = mid - 1
+    # print(low, high)
+    return nums[high]+k-(nums[high]-(high+1))
+
+nums = [6,8,9]
+k = 6
+print(missingNum2(nums, k, 0, len(nums)-1))
