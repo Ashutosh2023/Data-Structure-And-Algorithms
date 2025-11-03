1c55033cc9ccc4ffea0e69d0ba05d9c58c7b2bc7
“Ashutosh <singhashutoshkumar2023@gmail.com>
2025-11-03 06:40:05 +0530

    day 40 xor bit map brute solution

--- Day40_LC1486E_XORoperationInArray.py
+++ Day40_LC1486E_XORoperationInArray.py
@@ -20,3 +20,12 @@ class Solution:
         return result
     
 print(Solution().xorOperation(5,0))
+
+
+def xorOperation(n: int, start: int) -> int:
+    result = 0
+    for i in range(n):
+        result ^= start + 2 * i
+    return result
+
+print(xorOperation(4, 3))
