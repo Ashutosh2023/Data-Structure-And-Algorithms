281b068f9ff8c13403aa0082d2c305e5fee2249a
“Ashutosh <singhashutoshkumar2023@gmail.com>
2025-11-06 20:55:24 +0530

    day 45 maxdepth binary tree

---
+++ Day45_LC104E_MaxDepthOfBinaryTree.py
@@ -0,0 +1,49 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+# Create nodes
+root = Node(10)
+leftChild = Node(5)
+rightChild = Node(15)
+leftLeft = Node(2)
+leftRight = Node(7)
+leftRightLeft = Node(6)
+rightRight = Node(20)
+
+# Connect nodes to form the tree
+root.left = leftChild
+root.right = rightChild
+
+leftChild.left = leftLeft
+leftChild.right = leftRight
+
+leftRight.left = leftRightLeft
+
+rightChild.right = rightRight
+
+
+#           Root
+#           (10)
+#          /    \
+#       (5)      (15)
+#      /  \         \
+#   (2)   (7)       (20)
+#         /
+#       (6)
+
+
+
+# Depth First // PreOrder Traversal (Root::Left::Right) :
+def maxDepth(root: Node):
+    if not root:
+        return 0
+    left_depth = maxDepth(root.left)
+    right_depth = maxDepth(root.right)
+
+    return 1 + max(left_depth, right_depth)
+
+print(maxDepth(root))
