8b140b0668a80d08feb857977da77f5c69e267ff
“Ashutosh <singhashutoshkumar2023@gmail.com>
2025-11-09 23:18:55 +0530

    Day46 ZigZag Traversal Binary Tree

---
+++ Day46_LC103M_TreeZigZagTraversal.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+# Create nodes
+root = Node(10)
+leftChild = Node(5)
+rightChild = Node(15)
+leftLeft = Node(2)
+leftRight = Node(7)
+leftRightLeft = Node(6)
+rightRight = Node(20)
+
+# Connect nodes to form the tree
+root.left = leftChild
+root.right = rightChild
+
+leftChild.left = leftLeft
+leftChild.right = leftRight
+
+leftRight.left = leftRightLeft
+
+rightChild.right = rightRight
+
+
+#           Root
+#           (10)
+#          /    \
+#       (5)      (15)
+#      /  \         \
+#   (2)   (7)       (20)
+#         /
+#       (6)
+
+
+def zigzagLevelOrder(root: Node) -> list[list[int]]:
+    pass
+
+
+print(zigzagLevelOrder(root))
